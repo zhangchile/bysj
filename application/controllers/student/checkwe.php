f6fc974b80e62775e6c69b9632a92e3583c86b26
zhangchile <zzlchile@gmail.com>
2015-04-21 21:12:21 +0800

    加入highcharts图表.

--- application/controllers/student/checkwe.php
+++ application/controllers/student/checkwe.php
@@ -2,13 +2,15 @@
 
 class Checkwe extends CI_Controller {
 
+    public $_sid; 
+
     public function __construct()
     {
         parent::__construct();
         $this->load->model("acl_model");
         $this->load->model("notice_model");
         $this->config->load('pager_config',TRUE);
-
+        $this->_sid = $this->session->userdata('sid');
     }
 
     public function index()
--- application/views/student/checkwe.php
+++ application/views/student/checkwe.php
@@ -7,15 +7,60 @@
       <div class="col-md-8">
         <div class="panel panel-default">
         <!-- Default panel contents -->
-        <div class="panel-heading">通知
+        <div class="panel-heading">用水统计
           <!-- <a href="#" style="float:right;">查看所有>></a> -->
         </div>
-<!--         <div class="panel-body">
-          <p>公告啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊</p>
-        </div> -->
+        <div class="panel-body" id="wcontainer">
+          
+        </div>
       </div>
 
       </div>
       </div><!--/row-->
+
+<script type="text/javascript">
+$(function () {
+ $('#wcontainer').highcharts({
+        title: {
+            text: '用水量图'
+        },
+        subtitle: {
+            text: ''
+        },
+        xAxis: {
+            categories: ['04-05','04-06','04-07','04-08','04-09'],
+            title: {
+              text: '日期'
+            }
+        },
+        yAxis: {
+            min: 8,
+            tickInterval:0.5,
+            title: {
+                text: 'm³（吨）'
+            }
+        },
+        tooltip: {
+            crosshairs: true,
+            shared: true,
+            valueSuffix: ' m³（吨）'
+        },
+        series: [{
+            type: 'column',
+            name: '你',
+            data: [12,10,15,16,14],
+        }, {
+            type: 'spline',
+            name: '平均',
+            data: [12,12,12,12,12],
+            marker: {
+                lineWidth: 2,
+                lineColor: Highcharts.getOptions().colors[3],
+                fillColor: 'white'
+            }
+        }]
+    });
+});
+</script>
 
 <?php $this->load->view("template/footer.php");?>

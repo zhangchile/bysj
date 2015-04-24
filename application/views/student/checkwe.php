7bfc55a66dab2445dbea03ca17127bdafcedb038
zhangchile <zzlchile@gmail.com>
2015-04-24 20:26:49 +0800

    update.

--- application/views/student/checkwe.php
+++ application/views/student/checkwe.php
@@ -15,6 +15,16 @@
         </div>
       </div>
 
+        <div class="panel panel-default">
+        <!-- Default panel contents -->
+        <div class="panel-heading">用电统计
+          <!-- <a href="#" style="float:right;">查看所有>></a> -->
+        </div>
+        <div class="panel-body" id="econtainer">
+          
+        </div>
+      </div>
+
       </div>
       </div><!--/row-->
 
@@ -44,6 +54,48 @@ $(function () {
             crosshairs: true,
             shared: true,
             valueSuffix: ' m³（吨）'
+        },
+        series: [{
+            type: 'column',
+            name: '你',
+            data: [12,10,15,16,14],
+        }, {
+            type: 'spline',
+            name: '平均',
+            data: [12,12,12,12,12],
+            marker: {
+                lineWidth: 2,
+                lineColor: Highcharts.getOptions().colors[3],
+                fillColor: 'white'
+            }
+        }]
+    });
+
+
+  $('#econtainer').highcharts({
+        title: {
+            text: '用电量图'
+        },
+        subtitle: {
+            text: ''
+        },
+        xAxis: {
+            categories: ['04-05','04-06','04-07','04-08','04-09'],
+            title: {
+              text: '日期'
+            }
+        },
+        yAxis: {
+            min: 8,
+            tickInterval:0.5,
+            title: {
+                text: 'KW/h（度）'
+            }
+        },
+        tooltip: {
+            crosshairs: true,
+            shared: true,
+            valueSuffix: ' KW/h（度）'
         },
         series: [{
             type: 'column',
